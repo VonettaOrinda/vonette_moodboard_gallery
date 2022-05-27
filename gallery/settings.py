7d290b3b604064a0640c55ec91a4df603eec9a6f
VONETTA <thevonetteway@gmail.com>
2022-05-27 16:39:13 +0300

    added redirect to welcome.html and extended base in pictures.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pictures.apps.PicturesConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import gallery
 
 # Create your views here.
+
+def welcome(request):
+    return render(request,'all-pictures/welcome.html')
+
+
+# def home(request):
+    # gallery = gallery.objects.all()
+    # location = Location.objects.all()
+    # category = categories.objects.all()
