7855eee4ec26f29b1e9f7afb3483db0d0f0c6d28
VONETTA <thevonetteway@gmail.com>
2022-05-29 03:12:28 +0300

    changed timezone,added changes to databases

--- gallery/settings.py
+++ gallery/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        ' NAME':'gallery',
+        'USER':'vonetta',
+    'PASSWORD':'von123',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
