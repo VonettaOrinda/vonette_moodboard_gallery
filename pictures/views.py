3f935bd049af167863564672100ad2f94f8e992c
VONETTA <thevonetteway@gmail.com>
2022-05-29 17:59:40 +0300

    add changes made to views.py

--- pictures/views.py
+++ pictures/views.py
@@ -9,7 +9,7 @@ def welcome(request):
     return render(request,'all-pictures/welcome.html')
 
 
-def home(request):
-    pictures = Pictures.objects.all()
-    location = Location.objects.all()
-    category = Categories.objects.all()
+# def home(request):
+#     pictures = Pictures.objects.all()
+#     location = Location.objects.all()
+#     category = Categories.objects.all()
